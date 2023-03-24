654ad11516ddeb624674273aa5fef7a7e5b12c35
MedOK99 <127406946+MedOK99@users.noreply.github.com>
2023-03-24 22:33:05 +0300

    Бьюсь над решением задачи 007

---
+++ Task007/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая на 
+// вход принимает число (N), а на выходе 
+// показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Введите число: ");
+
+int a = int.Parse(Console.ReadLine());
+
+int min = 1;
+int max = a;
+int r = a % 2;
+
+while (min = 1; min <= max; min++);
+{
+  if (r = 0) ;
+  {
+    Console.WriteLine(min);
+  }
+}
+
+
+
